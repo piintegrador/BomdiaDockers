202a8eb413311758d3408c32d1ec2c80de47c2be
piintegrador <olajimdesign@gmail.com>
2018-07-01 03:08:40 -0300

    Add files via upload

---
+++ bomdia.py
@@ -0,0 +1,5 @@
+#arquivo bomdia.py
+import os
+import time
+
+print("Bom dia",os.environ['NOME'], "a data de hoje é:", time.strftime("%d/%m/%Y"))
---
+++ executar.sh
@@ -0,0 +1,5 @@
+#build da imagem
+docker build -t  -avaliacao05_jimmy_renan- .
+
+#criar container
+docker run -it -avaliacao05_jimmy_renan- 
